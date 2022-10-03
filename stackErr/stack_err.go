a11b4eee938093eb1f8aaa8775dafaaa01bf6078
Jeff Jiang <jeff.jiang.cd.cn@gmail.com>
2022-10-03 22:05:45 +0800

    errorWithStack类导出

--- stackErr/stack_err.go
+++ stackErr/stack_err.go
@@ -4,36 +4,36 @@ import "runtime"
 
 const DefaultErrorStackSkip = 1
 
-type errorWithStack struct {
+type ErrorWithStack struct {
 	err  error
 	file string
 	line int
 }
 
-func (e errorWithStack) Error() string {
+func (e ErrorWithStack) Error() string {
 	return e.err.Error()
 }
 
-func (e errorWithStack) Unwrap() error {
+func (e ErrorWithStack) Unwrap() error {
 	return e.err
 }
 
-func (e errorWithStack) File() string {
+func (e ErrorWithStack) File() string {
 	return e.file
 }
 
-func (e errorWithStack) Line() int {
+func (e ErrorWithStack) Line() int {
 	return e.line
 }
 
 func WithStack(err error, skip int) error {
 	switch err.(type) {
-	case errorWithStack:
+	case ErrorWithStack:
 		return err
 	default:
 		_, file, line, ok := runtime.Caller(skip)
 		if ok {
-			return errorWithStack{
+			return ErrorWithStack{
 				err:  err,
 				file: file,
 				line: line,
